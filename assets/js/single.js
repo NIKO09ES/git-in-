90f6c576eb6432e89c41f02b5593a76718c016eb
NIKO09ES <nicolasesquivelruiz@gmail.com>
2020-06-23 14:27:46 -0500

    add single.js file and code

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
 
 var displayIssues = function (issues) {
     if (issues.length === 0) {
@@ -13,25 +26,25 @@ var displayIssues = function (issues) {
         issueEl.setAttribute("target", "_blank");
 
         // create span to hold issue title
-    var titleEl = document.createElement("span");
-    titleEl.textContent = issues[i].title;
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
 
-    // append to container
-    issueEl.appendChild(titleEl);
+        // append to container
+        issueEl.appendChild(titleEl);
 
-    // create a type element
-    var typeEl = document.createElement("span");
+        // create a type element
+        var typeEl = document.createElement("span");
 
-    // check if issue is an actual issue or a pull request
-    if (issues[i].pull_request) {
-        typeEl.textContent = "(Pull request)";
-    } else {
-        typeEl.textContent = "(Issue)";
-    }
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+            typeEl.textContent = "(Pull request)";
+        } else {
+            typeEl.textContent = "(Issue)";
+        }
 
-    // append to container
-    issueEl.appendChild(typeEl);
-    
+        // append to container
+        issueEl.appendChild(typeEl);
+
     }
 
     issueContainerEl.appendChild(issueEl);
@@ -45,8 +58,12 @@ var getRepoIssues = function (repo) {
         // request was successful
         if (response.ok) {
             response.json().then(function (data) {
-                // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo)
+                }
             });
         }
         else {
